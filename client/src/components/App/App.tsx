a225f8ccb4decb89292bff70974048020ce6ba6a
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-02 19:19:44 +0300

    add router

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "@pages/system/MainPage/MainPage";
 import '@styles/index.scss';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: 'Promo page',
+    },
+    {
+        path: "system",
+        element: <MainPage />,
+        children: []
+    }
+]);
 
 const App: React.FC = () => {
     return (
-        <div>App</div>
+        <RouterProvider router={router} />
     )
 }
 
