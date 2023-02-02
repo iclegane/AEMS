0018d862aac6c35bd2937786e3aae7bf88c65454
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-02 19:20:17 +0300

    add react Strict mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import App from "@components/App/App";
+import App from "./components/App/App";
 
 const domNode = document.getElementById('root') as Element;
 const root = ReactDOM.createRoot(domNode);
 
 root.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
