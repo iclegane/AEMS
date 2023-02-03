331310f57d3f69938f8385700cc04eb07529c105
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-03 17:06:43 +0300

    added Dasgboard

---
+++ client/src/components/DashboardMenu/DashboardMenu.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './index.scss';
+
+const DashboardMenu: React.FC = () => {
+    return (
+        <>
+            <div className="dashboard-menu">
+                <div className="dashboard-menu__block">
+                    <div className="dashboard-menu__title">Главное меню</div>
+                    <div className="dashboard-menu__item-list">
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Главная</span>
+                        </a>
+
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Профиль</span>
+                        </a>
+
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Задачи</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="dashboard-menu__block">
+                    <div className="dashboard-menu__title">Компания</div>
+                    <div className="dashboard-menu__item-list">
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Календарь</span>
+                        </a>
+
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Сотрудники</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="dashboard-menu__block">
+                    <div className="dashboard-menu__title">Общее</div>
+                    <div className="dashboard-menu__item-list">
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Файлы</span>
+                        </a>
+
+                        <a href="#" className='link dashboard-menu__item'>
+                            <span className="link__icon"></span>
+                            <span className='link__text'>Настройки</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DashboardMenu;
