dc9571f746456273a41e4d5b0b9a7a627c1add35
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-04 14:58:29 +0300

    added icon component

---
+++ client/src/components/Icon/Icon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// todo: fix ts configuration for svg alias
+// @ts-ignore
+import Icons from '@assets/icons/interface/interface-sprite.svg';
+import {IIcon} from "./types";
+
+export const Icon: React.FC<IIcon> = ({name}) => {
+    return (
+        <>
+            <svg className={'icon'}>
+                <use href={Icons + '#' + name}></use>
+            </svg>
+        </>
+    )
+}
