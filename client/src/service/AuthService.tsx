754c0df836c6b90912207c689ded18ff89edd855
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-10 22:38:21 +0300

    added auth service

---
+++ client/src/service/AuthService.tsx
@@ -0,0 +1,24 @@
+import TRANSPORT from "../http";
+import {AxiosResponse} from "axios"
+import {IAuthResponse} from "../models/response/AuthResponse";
+
+
+export default class AuthService {
+    static async login(email: string, password: string): Promise<AxiosResponse<IAuthResponse>> {
+        return TRANSPORT.post<IAuthResponse>('/user/login', {
+            email,
+            password
+        });
+    }
+
+    static async registration(email: string, password: string): Promise<AxiosResponse<IAuthResponse>> {
+        return TRANSPORT.post<IAuthResponse>('/user/registration', {
+            email,
+            password
+        });
+    }
+
+    static async logout(): Promise<void> {
+        return TRANSPORT.post('/logout');
+    }
+}
