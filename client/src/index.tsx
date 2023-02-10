364cf0637e6aab7c8890a88a56f09aac1e1d1c46
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-10 22:35:19 +0300

    added redux provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import {Provider} from "react-redux";
+import {setupStore} from "./store/store";
 
 const domNode = document.getElementById('root') as Element;
 const root = ReactDOM.createRoot(domNode);
 
+const store = setupStore();
+
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
