d6e77e89d08ac524d0355208e5a2bc32d5617362
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-10 22:35:54 +0300

    added auth transport

---
+++ client/src/http/index.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import {API_URL} from "@utils/constants";
+
+
+const TRANSPORT = axios.create({
+    withCredentials: true,
+    baseURL: API_URL
+})
+
+TRANSPORT.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+
+    return config
+})
+
+export default TRANSPORT;
