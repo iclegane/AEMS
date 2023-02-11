ae7b61dea44bf42cd2313e3f858b2efeeb58c110
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-11 15:58:31 +0300

    added Profile page

---
+++ client/src/pages/system/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+
+export const ProfilePage: React.FC = () => {
+    return (
+        <div className="profile">
+
+            123321
+
+        </div>
+    );
+};
