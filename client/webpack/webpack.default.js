934e137bff4255068690ac65789c28bcb8e0172d
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-11 16:00:15 +0300

    added publicPath = '/' to output

--- client/webpack/webpack.default.js
+++ client/webpack/webpack.default.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname + '/../', 'dist'),
         filename: 'bundle-[hash].js',
+        publicPath: '/',
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.tsx', '.js'],
