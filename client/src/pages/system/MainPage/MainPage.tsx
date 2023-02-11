8024b5042a15452a052ac944f7dcf0ef1592b61b
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-11 15:58:53 +0300

    added <Outlet> to template

--- client/src/pages/system/MainPage/MainPage.tsx
+++ client/src/pages/system/MainPage/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '@layouts/DashboardLayout';
 import './index.scss';
+import { Outlet } from 'react-router-dom';
 
 export const SystemMainPage: React.FC = () => {
     return (
         <div className="page page--system">
             <Layout>
-                Page Content
+                <Outlet/>
             </Layout>
         </div>
     );
