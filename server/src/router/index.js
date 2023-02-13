02d0ea2619494d3cc798afdfa22d58ac2a8e6a06
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-13 14:21:26 +0300

    added new routes

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -18,6 +18,9 @@ router.post('/user/login', userController.login);
 router.post('/user/logout', userController.logout);
 router.post('/user/refresh', userController.refresh);
 
+router.post('/user/:id', userController.getUserInfo);
+router.put('/user/:id/edit', userController.updateUserInfo);
+
 router.get('/users', AuthMiddleware, userController.getUsers);
 
 
