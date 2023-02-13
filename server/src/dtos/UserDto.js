65310762bacf6862cbbcd347880ea7cc095fb1af
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-13 14:18:05 +0300

    added role name

--- server/src/dtos/UserDto.js
+++ server/src/dtos/UserDto.js
@@ -1,16 +1,17 @@
 class UserDto {
-    email;
-
     id;
-
+    email;
+    role;
+    post;
     isActivated;
 
     constructor(model) {
-        this.email = model.email;
         this.id = model._id;
+        this.role = model.role_id?.name || null;
+        this.email = model.email;
+        this.post = model.post;
         this.isActivated = model.isActivated;
     }
-
 }
 
 module.exports = UserDto;
