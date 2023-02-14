80e393f89a7d73dafdc01dc2293ea32858f2e827
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-14 22:48:20 +0300

    added auth interceptors

--- client/src/http/index.tsx
+++ client/src/http/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {API_URL} from '@utils/constants';
+import AuthService from "../service/AuthService";
 
 
 const TRANSPORT = axios.create({
@@ -11,6 +12,28 @@ TRANSPORT.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
 
     return config;
+});
+
+TRANSPORT.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await AuthService.refresh();
+
+            localStorage.setItem('token', response.data.accessToken);
+
+            return TRANSPORT.request((originalRequest));
+        } catch (e) {
+            console.log('user !auth')
+        }
+
+        throw error;
+    }
 });
 
 export default TRANSPORT;
