00b4442f857582b59879cf07f46dc59199caa760
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-15 23:51:00 +0300

    updated profile service

--- client/src/service/PofileService.tsx
+++ client/src/service/PofileService.tsx
@@ -1,25 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {API_URL} from "../utils/constants";
+import {RootState} from "../store/store";
 
+type Field = {
+    name: string;
+    value: string | null;
+}
 
 export interface ProfileResponse {
-    important: {
-        email: string;
-        phone: string | null;
-        work_date: string | null;
-        employment: string | null;
-        post: string | null;
-    },
-    personal: {
-        name: string | null;
-        email: string
-        birth_date: string | null;
-        gender: string | null;
-    },
-    contacts: {
-        address: string | null;
-        phone: string | null;
-    },
+    main: Field[],
+    personal: Field[],
+    contacts: Field[],
 }
 
 export interface ProfileRequest {
@@ -29,6 +20,16 @@ export interface ProfileRequest {
 export const ProfileService = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: API_URL,
+        prepareHeaders: (headers, {getState}) => {
+            const isAuth = (getState() as RootState).authReducer.auth.isAuth;
+
+            if (isAuth) {
+                headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+            }
+
+            return headers;
+        },
+        credentials: "include",
     }),
     endpoints: (builder) => ({
         profile: builder.query<ProfileResponse, ProfileRequest>({
