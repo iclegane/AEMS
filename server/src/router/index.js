04953a37ea01af7fc449da152807c8967bb46e73
Viktor <vvs-PathOfExile@yandex.ru>
2023-02-15 23:49:59 +0300

    updated

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const {body} = require('express-validator');
 const userController = require('../controllers/UserController');
+const profileController = require('../controllers/ProfileController');
 const AuthMiddleware = require('../middlewares/AuthMiddleware');
 
 
@@ -18,10 +19,12 @@ router.post('/user/login', userController.login);
 router.post('/user/logout', userController.logout);
 router.get('/user/refresh', userController.refresh);
 
-router.post('/user/:id', userController.getUserInfo);
-router.put('/user/:id/edit', userController.updateUserInfo);
+// router.post('/user/:id', userController.getUserInfo);
+// router.put('/user/:id/edit', userController.updateUserInfo);
+// router.get('/users', AuthMiddleware, userController.getUsers);
 
-router.get('/users', AuthMiddleware, userController.getUsers);
+
+router.post('/profile', AuthMiddleware, profileController.getProfileInfo);
 
 
 router.get('*', (req, res) => {
