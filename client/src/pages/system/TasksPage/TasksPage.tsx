3fb6592ac3f08a920b159f477a551a4179dd7ae6
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-02 13:51:06 +0300

    added Tasks page

---
+++ client/src/pages/system/TasksPage/TasksPage.tsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import './index.scss';
+import {sortTypes, useGetTasksQuery} from "../../../api/tasks";
+import Task from "@components/Task";
+import SortPanel from "../../../components/SortPanel";
+import Pagination from "../../../components/Pagination";
+
+
+export interface ISort {
+    field: string;
+    type: sortTypes;
+}
+
+export const TasksPage: React.FC = () => {
+
+    const [page, setPage] = useState(1);
+    const [limit] = useState(5);
+    const [sort, setSort] = useState<ISort | null>(null)
+
+
+    const { data, isLoading } = useGetTasksQuery({
+        page,
+        limit,
+        sort
+    });
+
+    if (isLoading) {
+        return <div>loading</div>
+    }
+
+    if (!data) {
+        return <div>Not found</div>
+    }
+
+    return (
+        <div className="tasks">
+            {!data && <div>Задач нет</div>}
+
+            <SortPanel sortManager={{sort,setSort}}/>
+
+            {data.tasks && data.tasks.map((task, index) =>
+                <Task
+                    id={task._id}
+                    key={task._id}
+                    index={++index}
+                    name={task.name}
+                    createdAt={task.createdAt}
+                    deadline={task.deadline}
+                    status={task.statusID.name}
+                    manager={task.managerID?.name || null}
+                />
+            )}
+
+            <Pagination
+                page={page}
+                pages={data.totalPage}
+                setPage={setPage}
+            />
+        </div>
+    );
+};
