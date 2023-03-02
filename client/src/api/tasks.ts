c1b5360dd3d8f81fb5c4c38c9db05063b58a5733
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-02 13:55:51 +0300

    added task inject endpoint

---
+++ client/src/api/tasks.ts
@@ -0,0 +1,63 @@
+import {api} from "./base";
+import {ISort} from "../pages/system/TasksPage/TasksPage";
+
+
+export interface Task {
+    _id: number;
+    name: string;
+    description: string;
+    createdAt: string;
+    deadline: string;
+    performerID: {
+        id: string;
+        name: string;
+    } | null;
+    managerID: {
+        id: string;
+        name: string;
+    } | null;
+    statusID: {
+        id: string;
+        name: string;
+    };
+}
+
+type TaskResponse = {
+    tasks: Task[];
+    totalPage: number;
+    page: number;
+    count: number;
+};
+
+export type sortTypes = 'asc' | 'desc' | null;
+
+export interface IGetTaskParams {
+    page: number;
+    limit: number;
+    sort: ISort | null
+}
+
+export const postsApi = api.injectEndpoints({
+    endpoints: (build) => ({
+        getTasks: build.query<TaskResponse, IGetTaskParams>({
+            query: (params) => {
+                const {sort, limit, page} = {...params}
+ 
+                return {
+                    url: 'tasks',
+                    params: {
+                        limit,
+                        page,
+                        sortField: sort?.field,
+                        sortType: sort?.type,
+                    }
+                }
+            }
+        })
+    })
+});
+
+
+export const {
+    useGetTasksQuery
+} = postsApi;
