9483da7ecebade8cabb53d96b8eb800180ea5c8e
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-04 22:50:48 +0300

    updated auth api

--- client/src/api/base.ts
+++ client/src/api/base.ts
@@ -1,9 +1,20 @@
 import {createApi, fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react';
 import {API_URL} from '../utils/constants';
+import {RootState} from "../store/store";
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: API_URL
+    baseUrl: API_URL,
+    prepareHeaders: (headers, {getState}) => {
+        const {isAuth} = (getState() as RootState).authReducer.auth;
+
+        if (isAuth) {
+            headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+        }
+
+        return headers;
+    },
+    credentials: 'include',
 });
 
 const baseQueryWithRetry = retry(baseQuery, { maxRetries: 3 });
@@ -11,10 +22,10 @@ const baseQueryWithRetry = retry(baseQuery, { maxRetries: 3 });
 export const api = createApi({
     reducerPath: 'splitApi',
     baseQuery: baseQueryWithRetry,
-    tagTypes: ['Tasks'],
-    endpoints: () => {return {};},
+    tagTypes: ['Tasks', 'Profile'],
+    endpoints: () => ({}),
 });
 
 export const enhancedApi = api.enhanceEndpoints({
-    endpoints: () => {return {};},
+    endpoints: () => ({}),
 });
