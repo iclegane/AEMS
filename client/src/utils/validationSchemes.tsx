81debe0b883aa69b6b8b17da33cccfbd51ed7743
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-07 22:37:55 +0300

    updated ProfilePersonalSchema

--- client/src/utils/validationSchemes.tsx
+++ client/src/utils/validationSchemes.tsx
@@ -15,15 +15,20 @@ export const SignInSchema = Yup.object().shape({
 
 export const ProfilePersonalSchema = Yup.object().shape({
     name: Yup.string()
-        .trim('xz')
+        .trim()
         .min(5, '2 символа минимально.')
         .max(30, '30 символов максимум.')
         .matches(/(?:[A-Za-z]+\s){2}[A-Za-z]+/, {
             message: 'ФИО должно состоять из 3 частей'
-        })
-        .required('Обязательное поле'),
-    birth_date: Yup.date()
-        .required('Обязательное поле'),
-    gender: Yup.string()
-        .required('Обязательное поле'),
+        }),
+    birth_date: Yup.date().min(new Date(1970, 0, 1)),
+    gender: Yup.string(),
+    address: Yup.string()
+        .trim()
+        .min(5, '5 символов минимально.')
+        .max(35, '35 cимволов максимально.'),
+    phone: Yup.string().matches(/^[(]?7{1}[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/, {
+        message: 'от 10 до 15 символов, состоит из цифр, начинается с 7.'
+    }),
+    underground: Yup.string()
 });
