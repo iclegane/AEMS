faa24b7b871c888f4ae82b85e0cce26a89a1bc4c
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-07 22:38:43 +0300

    updated types

--- server/src/service/Profile/types.ts
+++ server/src/service/Profile/types.ts
@@ -11,31 +11,15 @@ export interface IUserProfile extends IUserPopulated {
     address: IUserDocument['address'];
 }
 
-export interface IUserProfileDto {
-    important: {
-        employment: Omit<IUserProfile['employment_id'], 'name'> | null;
-        post: Omit<IUserProfile['post_id'], 'name'> | null;
-        skills: ISkillDB['name'][] | [];
-    }
-    personal: {
-        name: IUserProfile['name'];
-        birth_date: IUserProfile['birth_date'];
-        work_date: IUserProfile['work_date'];
-        gender: Omit<IUserProfile['gender_id'], 'name'> | null;
-        post: Omit<IUserProfile['post_id'], 'name'> | null;
-    }
-    contacts: {
-        address: IUserProfile['address'];
-        email: IUserProfile['email'];
-        phone: IUserProfile['phone'];
-        underground: Omit<IUserProfile['underground_id'], 'name'> | null;
-    }
-}
-
 export interface IUpdateProfileRequest {
     personal?: {
         name?: IUserProfile['name'];
         birth_date?: IUserProfile['birth_date'];
         gender?: IUserProfile['gender_id'];
+    }
+    contacts?: {
+        address?: IUserProfile['address']
+        phone?: IUserProfile['phone']
+        underground?: IUserProfile['underground_id']
     }
 }
