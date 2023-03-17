56155d769edf92bbdd48575a4a6aaf5bef267325
Viktor <vvs-PathOfExile@yandex.ru>
2023-03-17 15:32:02 +0300

    added menu

--- client/src/components/DashboardMenu/DashboardMenu.tsx
+++ client/src/components/DashboardMenu/DashboardMenu.tsx
@@ -2,72 +2,26 @@ import React from 'react';
 import Icon from '@components/Icon';
 import './index.scss';
 import {Link} from 'react-router-dom';
+import {menu} from "../../utils/menu";
+import {v4 as uuidv4} from "uuid";
 
 
 export const DashboardMenu: React.FC = () => {
     return (
         <div className="dashboard-menu">
-            <div className="dashboard-menu__block">
-                <div className="dashboard-menu__title">Главное меню</div>
-                <div className="dashboard-menu__item-list">
-                    <Link to="/system" className='link dashboard-menu__item'>
-                       <span className="link__icon">
-                            <Icon name="category"/>
-                        </span>
-                        <span className='link__text'>Главная</span>
-                    </Link>
-
-                    <Link to="/system/profile" className='link dashboard-menu__item'>
-                       <span className="link__icon">
-                            <Icon name="user"/>
-                        </span>
-                        <span className='link__text'>Профиль</span>
-                    </Link>
-
-                    <Link to="/system/tasks" className='link dashboard-menu__item'>
-                       <span className="link__icon">
-                            <Icon name="chart"/>
-                        </span>
-                        <span className='link__text'>Задачи</span>
-                    </Link>
+            {menu && menu.map((block) => (
+                <div key={uuidv4()} className="dashboard-menu__block">
+                    <div className="dashboard-menu__title">{block.title}</div>
+                    {block.items.map((menuItem) => (
+                        <Link to={menuItem.href} key={uuidv4()} className='link dashboard-menu__item'>
+                           <span className="link__icon">
+                                <Icon name={menuItem.icon} />
+                           </span>
+                            <span className='link__text'>{menuItem.text}</span>
+                        </Link>
+                    ))}
                 </div>
-            </div>
-            <div className="dashboard-menu__block">
-                <div className="dashboard-menu__title">Компания</div>
-                <div className="dashboard-menu__item-list">
-                    <a href="/" className='link dashboard-menu__item'>
-                        <span className="link__icon">
-                            <Icon name="calendar"/>
-                        </span>
-                        <span className='link__text'>Календарь</span>
-                    </a>
-
-                    <a href="/" className='link dashboard-menu__item'>
-                        <span className="link__icon">
-                            <Icon name="users"/>
-                        </span>
-                        <span className='link__text'>Сотрудники</span>
-                    </a>
-                </div>
-            </div>
-            <div className="dashboard-menu__block">
-                <div className="dashboard-menu__title">Общее</div>
-                <div className="dashboard-menu__item-list">
-                    <a href="/" className='link dashboard-menu__item'>
-                        <span className="link__icon">
-                            <Icon name="file"/>
-                        </span>
-                        <span className='link__text'>Файлы</span>
-                    </a>
-
-                    <a href="/" className='link dashboard-menu__item'>
-                        <span className="link__icon">
-                            <Icon name="setting"/>
-                        </span>
-                        <span className='link__text'>Настройки</span>
-                    </a>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
