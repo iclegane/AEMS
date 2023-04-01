1c2b4d5e0e6b3e26e5102bd4f3b092e7d0f09c60
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-01 22:24:25 +0300

    added usePageTitle

---
+++ client/src/hooks/page.tsx
@@ -0,0 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useMemo} from "react";
+import {setPageTitle} from "../store/reducers/TitleSlice";
+import {RootState} from "../store/store";
+
+
+export function usePageTitle(title: string) {
+    const dispatch = useDispatch();
+
+    const setTitle = useMemo(() => {
+        return (title: string) => dispatch(setPageTitle(title));
+    }, [dispatch]);
+
+    useEffect(() => {
+        setTitle(title);
+        return () => {
+            dispatch(setPageTitle(''));
+        };
+    }, [title, setTitle, dispatch]);
+
+    const pageTitle = useSelector((state: RootState) => state.pageTitle.title);
+
+    useEffect(() => {
+        document.title = pageTitle;
+    }, [pageTitle]);
+}
