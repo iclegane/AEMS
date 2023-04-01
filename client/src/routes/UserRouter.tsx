e4319fc42bdca7a7f221da3f584d9041435ca68b
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-01 22:26:36 +0300

    added SystemPage

--- client/src/routes/UserRouter.tsx
+++ client/src/routes/UserRouter.tsx
@@ -4,12 +4,12 @@ import TasksPage from '../pages/system/TasksPage';
 import TaskPage from '../pages/system/TaskPage';
 import AuthPage from '../pages/system/AuthPage';
 import ProfilePage from '../pages/system/ProfilePage';
-import MainPage from '../pages/system/MainPage';
 import VacationPage from '../pages/system/Vacation';
 import CalendarPage from "../pages/system/CalendarPage";
 import UsersPage from "../pages/system/UsersPage";
 import FilesPage from "../pages/system/FilesPage";
 import SettingsPage from "../pages/system/SettingsPage";
+import {SystemPage} from "../pages/system/SystemPage/SystemPage";
 
 
 const UserRouter = createBrowserRouter([
@@ -19,16 +19,16 @@ const UserRouter = createBrowserRouter([
         children: [
             {index: true, element: 'Promo page'},
             {path: 'login', element: <AuthPage />},
-            {path: 'system', element: <MainPage title="Main page" />,
+            {path: 'system', element: <SystemPage title="Главная страница" />,
                 children: [
-                    {path: 'profile', element: <ProfilePage/>},
-                    {path: 'tasks', element: <TasksPage/>},
-                    {path: 'tasks/:id', element: <TaskPage/>},
-                    {path: 'vacation', element: <VacationPage/>},
-                    {path: 'calendar', element: <CalendarPage/>},
-                    {path: 'users', element: <UsersPage/>},
-                    {path: 'files', element: <FilesPage/>},
-                    {path: 'settings', element: <SettingsPage/>},
+                    {path: 'profile', element: <ProfilePage title={'Профиль'}/>},
+                    {path: 'tasks', element: <TasksPage title={'Задачи'}/>},
+                    {path: 'tasks/:id', element: <TaskPage title={'Задача'}/>},
+                    {path: 'vacation', element: <VacationPage title={'Отпуск'}/>},
+                    {path: 'calendar', element: <CalendarPage title={'Календарь'}/>},
+                    {path: 'users', element: <UsersPage title={'Пользователи'}/>},
+                    {path: 'files', element: <FilesPage title={'Файлы'}/>},
+                    {path: 'settings', element: <SettingsPage title={'Настройки'}/>},
                 ]
             }
         ],
