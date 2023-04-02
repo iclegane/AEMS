9b63fd9169c177c21fa061e16a4231df350457a7
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-02 14:22:08 +0300

    added useRole

---
+++ client/src/hooks/useRole.ts
@@ -0,0 +1,10 @@
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import {RootState} from "../store/store";
+import {UserRole} from "../models/IUser";
+
+
+export const useRole = (): UserRole => {
+    const role = useSelector((state: RootState) => state.authReducer.auth.user?.role);
+    return useMemo(() => role || 'Guest', [role]);
+};
