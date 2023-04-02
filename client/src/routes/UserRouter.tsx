e011680b37b1784a4f96d7b278260a8d2a89b3d4
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-02 14:22:26 +0300

    removed ch routes

--- client/src/routes/UserRouter.tsx
+++ client/src/routes/UserRouter.tsx
@@ -6,8 +6,6 @@ import AuthPage from '../pages/system/AuthPage';
 import ProfilePage from '../pages/system/ProfilePage';
 import VacationPage from '../pages/system/Vacation';
 import CalendarPage from '../pages/system/CalendarPage';
-import UsersPage from '../pages/system/UsersPage';
-import FilesPage from '../pages/system/FilesPage';
 import SettingsPage from '../pages/system/SettingsPage';
 import {SystemPage} from '../pages/system/SystemPage/SystemPage';
 
@@ -26,8 +24,6 @@ const UserRouter = createBrowserRouter([
                     {path: 'tasks/:id', element: <TaskPage title="Задача"/>},
                     {path: 'vacation', element: <VacationPage title="Отпуск"/>},
                     {path: 'calendar', element: <CalendarPage title="Календарь"/>},
-                    {path: 'users', element: <UsersPage title="Пользователи"/>},
-                    {path: 'files', element: <FilesPage title="Файлы"/>},
                     {path: 'settings', element: <SettingsPage title="Настройки"/>},
                 ]
             }
