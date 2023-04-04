e9b6cf7c408633d2de23c3aad1c1792ddd0d8fa5
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-04 21:29:15 +0300

    added UserController

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,5 +1,42 @@
-class UserController {
+import {NextFunction, Request, Response} from 'express';
+import UserService from '../service/User/UserService.js';
 
+
+
+interface User {
+    name: string;
+    email: string;
+    role: string;
+    post: string;
+    password: string;
+}
+
+interface UserRequest extends Request {
+    body: User;
+}
+
+class UserController {
+    list = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const users = await UserService.getAllUsers();
+
+            res.json(users);
+        } catch (e) {
+            next(e);
+        }
+    };
+
+    add = async (req: UserRequest, res: Response, next: NextFunction) => {
+        try {
+            const {name, email, role, post, password} = req.body;
+
+            const user = await UserService.addUser(name, email, password, role, post);
+
+            res.json(user);
+        } catch (e) {
+            next(e);
+        }
+    };
 }
 
 export default new UserController();
---
+++ server/src/service/User/UserService.ts
@@ -0,0 +1,78 @@
+import bcrypt from 'bcrypt';
+import UserDto from '../../dtos/UserDto.js';
+import UserModel from '../../models/user/UserModel.js';
+import {IRoleDocument} from '../../models/role/types.js';
+import RoleModel from '../../models/role/RoleModel.js';
+import {IPostDocument} from '../../models/post/types.js';
+import PostModel from '../../models/post/PostModel.js';
+import ApiError from '../../exceptions/ApiError.js';
+
+
+class UserService {
+    async getAllUsers(): Promise<UserDto[]> {
+         const users = await UserModel.find()
+             .populate<{role_id: IRoleDocument}>({path: 'role_id', model: RoleModel})
+             .populate<{post_id: IPostDocument}>({path: 'post_id', model: PostModel}).exec();
+
+        if (!users.length) {
+            return [];
+        }
+
+        return users.map((user) => new UserDto(user));
+    }
+
+    async getUserById(id: string): Promise<UserDto> {
+        const user = await UserModel.findById(id)
+            .populate<{role_id: IRoleDocument}>({path: 'role_id', model: RoleModel})
+            .populate<{post_id: IPostDocument}>({path: 'post_id', model: PostModel}).exec();
+        if (!user) throw ApiError.BadRequest('User not found');
+
+        return new UserDto(user);
+    }
+
+    async addUser(name: string, email: string, password: string, role: string, post: string) {
+        const candidate = await UserModel.findOne({email}).exec();
+        if (candidate) {
+            throw ApiError.BadRequest(`Пользователь с таким email - ${email} уже существует`);
+        }
+
+        const hashPassword = await bcrypt.hash(password, 4).catch((err: Error) => {
+            throw new Error(`Error hashing password: ${err.message}`);
+        });
+
+        if (role === 'Admin') {
+            throw ApiError.BadRequest('Cannot create user with Admin role');
+        }
+
+        const roleObj = await RoleModel.findOne({ name: role });
+        if (!roleObj) {
+            throw ApiError.BadRequest(`Role not found: ${role}`);
+        }
+
+        // const postObj = await PostModel.findOne({ name: post });
+        // if (!postObj) {
+        //     throw ApiError.BadRequest(`Post not found: ${post}`);
+        // }
+
+        const update = {
+            email,
+            name,
+            // post,
+            password: hashPassword,
+            role_id: roleObj.id as string,
+        };
+
+        const options = { upsert: true, new: true };
+        const createdUser = await UserModel.findOneAndUpdate({ email }, update, options);
+
+        if (!createdUser) {
+            throw new Error(`Failed to create user with email: ${email}`);
+        }
+
+        const userDto = this.getUserById(createdUser.id as string);
+
+        return userDto;
+    }
+}
+
+export default new UserService();
