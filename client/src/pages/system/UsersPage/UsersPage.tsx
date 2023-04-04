6a212d6011d06d65510b7cb1439d6319f6e92958
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-04 21:28:53 +0300

    added UsersTable CustomModal

--- client/src/pages/system/UsersPage/UsersPage.tsx
+++ client/src/pages/system/UsersPage/UsersPage.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.scss';
 import Page from '../../../components/Page';
+import UsersTable from "../../../components/Tables/UsersTable";
+import {CustomModal} from "../../../components/CustomModal/CustomModal";
+import {AddUserForm} from "../../../components/Forms/Users/AddUserForm/AddUserForm";
 
 
-interface UsersPageProps {
-    title: string;
-}
+export const UsersPage: React.FC<{ title: string }> = ({ title}) => {
 
-export const UsersPage: React.FC<React.PropsWithChildren<UsersPageProps>> = ({ title, children, ...rest }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <Page title={title}>
-            <div className="users gap-30">
-                <div className="dashboard-content-block" />
+            <div className="users flex flex-column gap-30">
+                <div className="dashboard-content-block">
+                    <button onClick={handleClick} className={'button button--default'}>Добавить пользователя</button>
+                </div>
+                <div className="dashboard-content-block">
+                    <UsersTable/>
+                </div>
             </div>
+            <CustomModal modalState={{ isOpen, setIsOpen }}>
+                <AddUserForm/>
+            </CustomModal>
         </Page>
     );
 };
