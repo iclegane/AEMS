fcf788351f45434371561685023c41e8737cbbaf
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-04 21:02:30 +0300

    added AddUserForm

---
+++ client/src/components/Forms/Users/AddUserForm/types.ts
@@ -0,0 +1,19 @@
+import {IUser, UserRole} from "../../../../models/IUser";
+import {FetchBaseQueryError} from "@reduxjs/toolkit/query";
+import {SerializedError} from "@reduxjs/toolkit";
+
+export interface IAddUserFromFields extends Omit<Partial<IUser>, 'confirmPassword'> {
+    confirmPassword: string;
+    password: string;
+}
+
+export type AddUserFormSelectOption = {
+    readonly key: string;
+    readonly value: UserRole;
+    readonly label: string;
+}
+
+export interface AddUserResponse {
+    data?: IUser;
+    error?: FetchBaseQueryError | SerializedError | unknown;
+}
