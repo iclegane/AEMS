4b3ca337f903fbfd5c7db9399b227a0093ee4541
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-04 21:28:30 +0300

    added SignUpSchema

--- client/src/utils/validationSchemes.tsx
+++ client/src/utils/validationSchemes.tsx
@@ -13,6 +13,29 @@ export const SignInSchema = Yup.object().shape({
         .required('Обязательное поле'),
 });
 
+export const SignUpSchema = Yup.object().shape({
+    name: Yup.string()
+        .trim()
+        .min(5, '2 символа минимально.')
+        .max(30, '30 символов максимум.')
+        .matches(/(?:[A-Za-z]+\s){2}[A-Za-z]+/, {
+            message: 'ФИО должно состоять из 3 частей'
+        })
+        .required('Обязательное поле'),
+    email: Yup.string().email('Почта должна соответсвтовать - example@email.com')
+        .min(2, '2 символа минимально.')
+        .max(30, '30 символов максимум.')
+        .required('Обязательное поле'),
+    role: Yup.string().required('Обязательное поле'),
+    password: Yup.string()
+        .min(5, '5 символов минимально.')
+        .max(30, '30 символов максимум.')
+        .required('Обязательное поле'),
+    confirmPassword: Yup.string()
+        .required('Обязательное поле')
+        .oneOf([Yup.ref('password')], 'Пароль должен совпадать'),
+});
+
 export const ProfilePersonalSchema = Yup.object().shape({
     name: Yup.string()
         .trim()
