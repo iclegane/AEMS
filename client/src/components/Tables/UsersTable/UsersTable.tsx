90af9e8b00ca63ffc3c501cecb87656c72592138
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-05 15:33:01 +0300

    added splitted context form by event type

--- client/src/components/Tables/UsersTable/UsersTable.tsx
+++ client/src/components/Tables/UsersTable/UsersTable.tsx
@@ -1,70 +1,89 @@
 import React from 'react';
-import {Table} from 'antd';
+import {Table, Space} from 'antd';
 import {ColumnsType} from 'antd/es/table';
 import {IUser} from '../../../models/IUser';
 import {useGetUsersQuery} from '../../../api/users';
+import {formActions} from '../../../pages/system/UsersPage';
 
 
-const columns: ColumnsType<IUser> = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-        width: '15%',
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        width: '20%',
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortDirections: ['descend', 'ascend'],
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-        width: '20%',
-        sorter: (a, b) => a.email.length - b.email.length,
-        sortDirections: ['descend', 'ascend'],
-    },
-    {
-        title: 'Post',
-        dataIndex: 'post',
-        key: 'post',
-        width: '20%'
-    },
-    {
-        title: 'Role',
-        dataIndex: 'role',
-        key: 'role',
-        width: '20%',
-        filters: [
-            {
-                text: 'User',
-                value: 'User',
-            },
-            {
-                text: 'Manager',
-                value: 'Manager',
-            },
-            {
-                text: 'Admin',
-                value: 'Admin',
-            },
-        ],
-        onFilter: (value: string | number | boolean, record) => record.role.startsWith(value.toString()),
-        filterSearch: true,
-    },
-];
-
-export const UsersTable: React.FC = () => {
+export const UsersTable: React.FC<{actionHandler: (action: formActions, id: string) => () => void}> = ({actionHandler}) => {
 
     const { data: users = [], isLoading, isError } = useGetUsersQuery();
 
     if (isError) {
         return <p>Error loading users.</p>;
     }
+
+    const handleAddTaskButtonClick = (id: string): React.MouseEventHandler<HTMLButtonElement> => (event) => {
+        event.preventDefault();
+
+        const handleAction = actionHandler('add-task', id);
+        handleAction();
+    };
+
+    const columns: ColumnsType<IUser> = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            width: '15%',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            width: '20%',
+            sorter: (a, b) => a.name.length - b.name.length,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            width: '10%',
+            sorter: (a, b) => a.email.length - b.email.length,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Post',
+            dataIndex: 'post',
+            key: 'post',
+            width: '10%'
+        },
+        {
+            title: 'Role',
+            dataIndex: 'role',
+            key: 'role',
+            width: '10%',
+            filters: [
+                {
+                    text: 'User',
+                    value: 'User',
+                },
+                {
+                    text: 'Manager',
+                    value: 'Manager',
+                },
+                {
+                    text: 'Admin',
+                    value: 'Admin',
+                },
+            ],
+            onFilter: (value: string | number | boolean, record) => record.role.startsWith(value.toString()),
+            filterSearch: true,
+        },
+        {
+            title: 'Actions',
+            dataIndex: 'actions',
+            key: 'actions',
+            width: '20%',
+            render: (_, record) => (
+                <Space size="middle">
+                    <button onClick={handleAddTaskButtonClick(record.id)} type="button" className="button button--default">Добавить задачу</button>
+                </Space>
+            ),
+        },
+    ];
 
     const usersWithKeys = users.map((user) => ({ ...user, key: user.id }));
 
--- client/src/pages/system/UsersPage/UsersPage.tsx
+++ client/src/pages/system/UsersPage/UsersPage.tsx
@@ -1,31 +1,46 @@
-import React, {useState} from 'react';
-import './index.scss';
+import React, {useCallback, useMemo, useState} from 'react';
 import Page from '../../../components/Page';
 import UsersTable from '../../../components/Tables/UsersTable';
 import {CustomModal} from '../../../components/CustomModal/CustomModal';
-import {AddUserForm} from '../../../components/Forms/Users/AddUserForm/AddUserForm';
+import AddUserForm from '../../../components/Forms/Users/AddUserForm';
+import AddTaskForm from '../../../components/Forms/Users/AddTaskForm';
+import {formActions} from './types';
+import './index.scss';
 
 
 export const UsersPage: React.FC<{ title: string }> = ({ title}) => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [modalContent, setModalContent] = useState<JSX.Element | null>(null);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-    };
+    const handleButtonClick = useCallback((form: formActions, taskId: string | null = null): void => {
+        if (form === 'add-user') {
+            setModalContent(<AddUserForm />);
+        } else if (form === 'add-task') {
+            if (taskId)
+                setModalContent(<AddTaskForm taskID={taskId} />);
+        }
+        setIsOpen(true);
+    }, [setIsOpen, setModalContent]);
+
+    const handleAddTaskClick = (action: formActions, taskId: string): (() => void) => () => handleButtonClick('add-task', taskId);
+
+    const memoizedModalContent = useMemo(() => modalContent, [modalContent]);
+
+    const memoizedHandleAddTaskClick = useCallback(handleAddTaskClick, []);
 
     return (
         <Page title={title}>
             <div className="users flex flex-column gap-30">
                 <div className="dashboard-content-block">
-                    <button onClick={handleClick} className="button button--default">Добавить пользователя</button>
+                    <button type="button" onClick={() => handleButtonClick('add-user')} className="button button--default">Добавить пользователя</button>
                 </div>
                 <div className="dashboard-content-block">
-                    <UsersTable/>
+                    <UsersTable actionHandler={memoizedHandleAddTaskClick} />
                 </div>
             </div>
             <CustomModal modalState={{ isOpen, setIsOpen }}>
-                <AddUserForm/>
+                {memoizedModalContent}
             </CustomModal>
         </Page>
     );
