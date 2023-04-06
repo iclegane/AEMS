e10c3370e67b11533b9c49e44649d9dad7e0ccb3
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-06 14:16:06 +0300

    created a separate model

---
+++ client/src/models/ITask.tsx
@@ -0,0 +1,37 @@
+import { ISort } from './ISort';
+
+
+export interface Task {
+    id: string;
+    name: string;
+    description: string;
+    created: string;
+    updated: string;
+    deadline: string;
+    body: string,
+    performer?: string;
+    manager?: string;
+    status?: string;
+}
+
+export interface TaskListResponseData {
+    tasks: Task[];
+    totalPage: number;
+    page: number;
+    count: number;
+}
+
+export interface GetTasksParams {
+    page: number;
+    limit: number;
+    sort: ISort | null
+}
+
+export interface CreateTaskParamsData {
+    name: string,
+    description: string,
+    deadline: string,
+    body: string,
+    performerID: string,
+    managerID: string,
+}
