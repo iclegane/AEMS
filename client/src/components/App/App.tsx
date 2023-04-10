c8381340a6f5ece47c8cddae48783e56f1330fd4
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-10 11:29:47 +0300

    added useRefreshAuth

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,31 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import { checkAuth } from '../../store/actions/AuthAction';
 import UserRouter from '../../routes/UserRouter';
 import '@styles/index.scss';
-import { RootState } from '../../store/store';
 import AdminRouter from '../../routes/AdminRouter';
 import { useRole } from '../../hooks/useRole';
 import UnauthorizedRouter from '../../routes/UnauthorizedRouter';
 import { useIsAuth } from '../../hooks/useIsAuth';
+import { useRefreshAuth } from '../../hooks/useRefreshAuth';
 
-
-let didInit = false;
-
+ 
 const AppConnect: React.FC = () => {
-    const dispatch = useAppDispatch();
     const isAuth = useIsAuth();
     const role = useRole();
-
-    useEffect(() => {
-        if (!didInit) {
-            didInit = true;
-            if (localStorage.getItem('token')) {
-                dispatch(checkAuth());
-            }
-        }
-    }, [dispatch]);
+    useRefreshAuth();
 
     if (isAuth) {
         const Router = role === 'Admin' ? AdminRouter : UserRouter;
@@ -35,12 +22,4 @@ const AppConnect: React.FC = () => {
     return <RouterProvider router={UnauthorizedRouter} />;
 };
 
-export const App: React.FC = () => {
-    const { isLoading } = useAppSelector((state: RootState) => state.authReducer);
-
-    if (isLoading) {
-        return null;
-    }
-
-    return <AppConnect />;
-};
+export const App: React.FC = () => <AppConnect />;
