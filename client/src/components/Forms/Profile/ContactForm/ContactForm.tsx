8e874642043fffdb607191957e91def18c2809ac
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-12 16:16:31 +0300

    updated ContactForm

--- client/src/components/Forms/Profile/ContactForm/ContactForm.tsx
+++ client/src/components/Forms/Profile/ContactForm/ContactForm.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { Select, Spin } from 'antd';
 import { ProfilePersonalSchema } from '../../../../utils/validationSchemes';
 import { useUpdateProfileMutation } from '../../../../api/profile';
 import { IContactForm } from './types';
 import { useGetUndergroundsQuery } from '../../../../api/underground';
+import ResponseMessage from '../../../ResponseMessage';
 
 
 export const ContactForm: React.FC<{data: IContactForm}> = (props) => {
 
     const { data } = props;
-    const { address, phone } = data;
+    const { address, phone, underground } = data;
 
-    const [UpdateProfile] =  useUpdateProfileMutation();
+    const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+
+    const [UpdateProfile, { isLoading: isUpdatingTask }] =  useUpdateProfileMutation();
     const { data: undergrounds = [], isLoading: isUndergroundsLoading, isError: isUndergroundsError } = useGetUndergroundsQuery({});
 
     if (isUndergroundsError) {
@@ -23,57 +26,64 @@ export const ContactForm: React.FC<{data: IContactForm}> = (props) => {
         initialValues: {
             address,
             phone,
-            underground: null,
+            underground: undergrounds.find((item) => item.name === underground)?.id || null,
         },
         initialStatus: false,
         validationSchema: ProfilePersonalSchema,
         onSubmit: async (formData) => {
-            UpdateProfile({
+            const response = UpdateProfile({
                 contacts: formData
             });
+
+            setIsSuccess(!('error' in response));
         },
     });
 
     return(
-        <form className='form' onSubmit={formik.handleSubmit}>
-            <div className='form-group'>
-                <label htmlFor="name">Адрес</label>
-                <input
-                    id='address'
-                    name='address'
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.address || ''}
-                />
-                {formik.errors.address ? <span className="form__err-msg">{formik.errors.address}</span> : null}
-            </div>
-            <div className='form-group'>
-                <label htmlFor="name">Телефон</label>
-                <input
-                    id='phone'
-                    name='phone'
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.phone || ''}
-                />
-                {formik.errors.phone ? <span className="form__err-msg">{formik.errors.phone}</span> : null}
-            </div>
-            <Spin spinning={isUndergroundsLoading}>
-                <label htmlFor="name">Метро</label>
-                <Select
-                    style={{ width: 120 }}
-                    value={formik.values.underground}
-                    onChange={(selectedOption) => {
-                        formik.setFieldValue('underground', selectedOption);
-                    }}
-                    options={undergrounds.map((underground) => ({
-                        label: underground.name,
-                        value: underground.id,
-                    }))}
-                />
-                {formik.errors.underground ? <span className="form__err-msg">{formik.errors.underground}</span> : null}
-            </Spin>
-            <button disabled={isUndergroundsLoading} type='submit' className='button button--blue form__submit'>Изменить</button>
-        </form>
+        <Spin spinning={isUpdatingTask}>
+            {isSuccess !== null && <ResponseMessage isSuccess={isSuccess}/>}
+            {isSuccess === null && (
+                <form className='form' onSubmit={formik.handleSubmit}>
+                    <div className='form-group'>
+                        <label htmlFor="name">Адрес</label>
+                        <input
+                            id='address'
+                            name='address'
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.address || ''}
+                        />
+                        {formik.errors.address ? <span className="form__err-msg">{formik.errors.address}</span> : null}
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="name">Телефон</label>
+                        <input
+                            id='phone'
+                            name='phone'
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.phone || ''}
+                        />
+                        {formik.errors.phone ? <span className="form__err-msg">{formik.errors.phone}</span> : null}
+                    </div>
+                    <Spin spinning={isUndergroundsLoading}>
+                        <label htmlFor="name">Метро</label>
+                        <Select
+                            style={{ width: 120 }}
+                            value={formik.values.underground}
+                            onChange={(selectedOption) => {
+                                formik.setFieldValue('underground', selectedOption);
+                            }}
+                            options={undergrounds.map((item) => ({
+                                label: item.name,
+                                value: item.id,
+                            }))}
+                        />
+                        {formik.errors.underground ? <span className="form__err-msg">{formik.errors.underground}</span> : null}
+                    </Spin>
+                    <button disabled={isUndergroundsLoading} type='submit' className='button button--blue form__submit'>Изменить</button>
+                </form>
+            )}
+        </Spin>
     );
 };
