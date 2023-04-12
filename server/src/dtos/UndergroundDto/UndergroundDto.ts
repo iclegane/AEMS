4398f2c17c08a17d28672450c647da85ced38378
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-12 15:49:48 +0300

    added UndergroundDto

--- server/src/dtos/UndergroundDto/UndergroundDto.ts
+++ server/src/dtos/UndergroundDto/UndergroundDto.ts
@@ -2,9 +2,12 @@ import {IUndergroundDocument} from '../../models/underground/types.js';
 
 
 class UndergroundDto {
+    id: string;
+
     name: string;
 
     constructor(undergroundModel: IUndergroundDocument) {
+        this.id = undergroundModel.id as string;
         this.name = undergroundModel.name;
     }
 }
