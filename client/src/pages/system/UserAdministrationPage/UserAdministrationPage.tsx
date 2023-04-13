8767d65e5aee3f22fd2ed585bc89c6664e3990b9
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-13 17:36:02 +0300

    added UserAdministrationPage

--- client/src/pages/system/UserAdministrationPage/UserAdministrationPage.tsx
+++ client/src/pages/system/UserAdministrationPage/UserAdministrationPage.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Page from '../../../components/Page';
 import './index.scss';
 import { PageProps } from '../../../models/IPage';
+import { useParams } from 'react-router-dom';
 
 
 export const UserAdministrationPage: React.FC<PageProps> = ({ title }) => {
+    const { id } = useParams();
 
 
     return (
