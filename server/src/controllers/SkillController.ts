a0ecdd9d2bed7a35204c29a0c0f18e899dfdaf71
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-13 14:45:43 +0300

    added SkillController

---
+++ server/src/controllers/SkillController.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from 'express';
+import SkillService from '../service/Skill/SkillService.js';
+
+
+class SkillController {
+    getSkills = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const skills = await SkillService.getAllPosts();
+            
+            res.json(skills);
+        } catch (e) {
+            next(e);
+        }
+    };
+}
+
+export default new SkillController();
