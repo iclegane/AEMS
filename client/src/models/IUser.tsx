6a01bb5a9dd2445bcd21258a16c760647d9c5596
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-13 18:55:13 +0300

    added UserInfoDto, UserInfoDtoField

--- client/src/models/IUser.tsx
+++ client/src/models/IUser.tsx
@@ -1,4 +1,3 @@
-
 export type UserRole = 'Admin' | 'User' | 'Manager' | 'Guest';
 
 export interface IUser {
@@ -7,4 +6,24 @@ export interface IUser {
     email:string;
     post: string | null;
     role: UserRole;
+}
+
+export type UserInfoDtoField = {
+    id: string;
+    name: string;
+}
+
+export interface UserInfoDto {
+    id: string;
+    name: string | null;
+    email: string;
+    address: string | null;
+    phone: string | null;
+    birth_date: Date | null;
+    work_date: Date;
+    post: UserInfoDtoField | null;
+    role: UserInfoDtoField | null;
+    gender: UserInfoDtoField | null;
+    skill: UserInfoDtoField[];
+    underground: UserInfoDtoField | null;
 }
