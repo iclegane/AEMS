6c3eaaebc850287b1081a26ba4098be0795ff134
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-13 19:09:56 +0300

    added UpdateUserForm

---
+++ client/src/components/Forms/Users/UpdateUserForm/UpdateUserForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const UpdateUserForm: React.FC = () => {
+
+    return(
+        <form className='form'>
+            <h2 className="text-center">Изменить данные пользователя</h2>
+        </form>
+    );
+};
