349d63054111f84a075832c759052455feab2f13
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-14 17:40:08 +0300

    added interface

---
+++ server/src/types/IUserApi.ts
@@ -0,0 +1,13 @@
+export interface IUpdateUserRequestData {
+    address: string;
+    birth_date: string;
+    work_date: string;
+    email: string;
+    gender: string;
+    name: string;
+    phone: string;
+    post: string;
+    role: string;
+    skill: string[];
+    underground: string;
+}
