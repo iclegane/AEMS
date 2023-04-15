4a3b68e28c7f5b3b66bc30fb7ab014edd773ebff
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-15 13:57:07 +0300

    removed addUser method

--- server/src/service/User/UserService.ts
+++ server/src/service/User/UserService.ts
@@ -1,4 +1,3 @@
-import bcrypt from 'bcrypt';
 import UserDto from '../../dtos/UserDto.js';
 import UserModel from '../../models/user/UserModel.js';
 import {IRoleDocument} from '../../models/role/types.js';
@@ -6,7 +5,6 @@ import RoleModel from '../../models/role/RoleModel.js';
 import {IPostDocument} from '../../models/post/types.js';
 import PostModel from '../../models/post/PostModel.js';
 import ApiError from '../../exceptions/ApiError.js';
-import SkillModel from '../../models/skill/SkillModel.js';
 
 
 class UserService {
@@ -29,56 +27,6 @@ class UserService {
         if (!user) throw ApiError.BadRequest('User not found');
 
         return new UserDto(user);
-    }
-
-    async addUser(name: string, email: string, password: string, role: string, post: string, skills: string[]) {
-        const candidate = await UserModel.findOne({email}).exec();
-        if (candidate) {
-            throw ApiError.BadRequest(`Пользователь с таким email - ${email} уже существует`);
-        }
-
-        const hashPassword = await bcrypt.hash(password, 4).catch((err: Error) => {
-            throw new Error(`Error hashing password: ${err.message}`);
-        });
-
-        if (role === 'Admin') {
-            throw ApiError.BadRequest('Cannot create user with Admin role');
-        }
-
-        const roleObj = await RoleModel.findOne({ name: role });
-        if (!roleObj) {
-            throw ApiError.BadRequest(`Role not found: ${role}`);
-        }
-
-        const postObj = await PostModel.findById(post);
-        if (!postObj) {
-            throw ApiError.BadRequest(`Post not found: ${post}`);
-        }        
-        
-        const skillsObj = await SkillModel.find({ _id: { $in: skills } });
-        if (!skillsObj.length) {
-            throw ApiError.BadRequest(`Skills not found: ${post}`);
-        }
-
-        const update = {
-            email,
-            name,
-            post,
-            password: hashPassword,
-            role_id: roleObj.id as string,
-            skill: skills,
-        };
-
-        const options = { upsert: true, new: true };
-        const createdUser = await UserModel.findOneAndUpdate({ email }, update, options);
-
-        if (!createdUser) {
-            throw new Error(`Failed to create user with email: ${email}`);
-        }
-
-        const userDto = this.getUserById(createdUser.id as string);
-
-        return userDto;
     }
 }
 
