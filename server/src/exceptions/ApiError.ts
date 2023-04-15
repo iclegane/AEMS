375d5035d2684639c2b950ee53009ed604321575
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-15 15:53:50 +0300

    updated ApiError

--- server/src/exceptions/ApiError.ts
+++ server/src/exceptions/ApiError.ts
@@ -1,23 +1,43 @@
-import {ValidationError} from 'express-validator';
+import {ValidationError} from 'yup';
 
 
 class ApiError extends Error {
-    status;
+    status: number;
 
-    errors;
+    errors: ValidationError[];
+
+    timestamp: Date;
 
     constructor(status: number, message: string, errors: ValidationError[] = []) {
         super(message);
         this.status = status;
         this.errors = errors;
+        this.timestamp = new Date();
     }
 
-    static UnauthorizedError() {
+    static UnauthorizedError(): ApiError {
         return new ApiError(401, 'Пользователь не авторизован');
     }
 
-    static BadRequest(message: string, errors: ValidationError[] = []) {
+    static BadRequest(message: string, errors: ValidationError[] = []): ApiError {
         return new ApiError(400, message, errors);
+    }
+
+    static NotFound(message: string, errors: ValidationError[] = []): ApiError {
+        return new ApiError(404, message, errors);
+    }
+
+    static InternalServer(message: string, errors: ValidationError[] = []): ApiError {
+        return new ApiError(500, message, errors);
+    }
+
+    static FromError(error: Error): ApiError {
+        return new ApiError(500, error.message);
+    }
+
+    logError() {
+        console.error(`${this.timestamp.toISOString()} - ${this.message}`);
+        console.error(this.errors);
     }
 }
 
