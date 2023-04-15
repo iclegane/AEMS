ee756ca8578931b1605a85b3daf8bbbde531255e
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-15 13:55:53 +0300

    updated getUserById and createUser

--- server/src/controllers/UserAdminController.ts
+++ server/src/controllers/UserAdminController.ts
@@ -1,11 +1,11 @@
 import {NextFunction, Request, Response} from 'express';
-import ApiError from '../exceptions/ApiError.js';
+import bcrypt from 'bcrypt';
 import UserAdminService from '../service/UserAdmin/UserAdminService.js';
-import {UserAdminUpdateSchema} from '../utils/validations.js';
-import {IUpdateUserRequestData} from '../types/IUserApi';
+import {UserAdminCreateSchema, UserAdminGetSchema, UserAdminUpdateSchema} from '../utils/validations.js';
+import {ICreateUserRequestData, IUpdateUserRequestData} from '../types/IUserApi';
 
 
- 
+
 class UserAdminController {
 
     getAllUsers = () => {};
@@ -13,9 +13,12 @@ class UserAdminController {
     getUserById = async (req: Request<never, never, { id: string }>, res: Response, next: NextFunction) => {
         try {
             const { id } = req.params;
-            if (!id) throw ApiError.BadRequest('ID not found');
 
-            const user = await UserAdminService.getUserByID(id);
+            const { userID } = await UserAdminGetSchema.validate({
+                userID : id
+            });
+
+            const user = await UserAdminService.getUserByID(userID);
 
             res.json(user);
         } catch (e) {
@@ -23,7 +26,26 @@ class UserAdminController {
         }
     };
 
-    createUser = () => {};
+    createUser = async (req: Request<never, never, { data: ICreateUserRequestData }>, res: Response, next: NextFunction) => {
+        try {
+            const {data} = req.body;
+
+            const createData = await UserAdminCreateSchema.validate(data);
+
+            const hashPassword = await bcrypt.hash(data.password, 4).catch((err: Error) => {
+                throw new Error(`Error hashing password: ${err.message}`);
+            });
+
+            const user = await UserAdminService.createUser({
+                ...createData,
+                password: hashPassword,
+            });
+
+            res.json(user);
+        } catch (e) {
+            next(e);
+        }
+    };
 
     updateUserById = async (
         req: Request<{ id: string }, never, { data: IUpdateUserRequestData }>,
