83bec4ea48b64e2e0b61eb5ee9f6a3ae0f6ad7a0
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-15 15:55:35 +0300

    removed AuthController.registration, added error handler

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,5 +1,4 @@
-import {Router, Response, Request} from 'express';
-import {body} from 'express-validator';
+import {Router, Response, Request, NextFunction} from 'express';
 import AuthController from '../controllers/AuthController.js';
 import UserController from '../controllers/UserController.js';
 import AuthMiddleware from '../middlewares/AuthMiddleware.js';
@@ -12,14 +11,13 @@ import SkillController from '../controllers/SkillController.js';
 import RoleController from '../controllers/RoleController.js';
 import GenderController from '../controllers/GenderController.js';
 import UserAdminController from '../controllers/UserAdminController.js';
+import ApiError from '../exceptions/ApiError.js';
+import errorMiddleware from '../middlewares/ErrorMiddleware.js';
 
 
 const router = Router();
 
-router.post('/auth/registration',
-    body('email').isEmail(),
-    body('password').isLength({min: 3, max: 32}),
-    AuthController.registration);
+
 router.post('/auth/login', AuthController.login);
 router.post('/auth/logout', AuthController.logout);
 router.get('/auth/refresh', AuthController.refresh);
@@ -36,7 +34,6 @@ router.get('/statuses',  TaskStatusController.list);
 
 router.get('/users/list', UserController.list);
 
-
 router.get('/underground', UndergroundController.list);
 router.get('/posts', PostController.getPosts);
 router.get('/skills', SkillController.getSkills);
@@ -47,6 +44,11 @@ router.get('/admin/users/:id', UserAdminController.getUserById);
 router.put('/admin/users/:id', UserAdminController.updateUserById);
 router.post('/admin/users/add', UserAdminController.createUser);
 
-router.get('*', (req: Request, res: Response) => res.sendStatus(404));
+router.get('*', (req: Request, res: Response) => {
+    throw ApiError.NotFound('Not Found');
+});
 
+router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    errorMiddleware(err, req, res);
+});
 export default router;
