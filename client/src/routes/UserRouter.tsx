2fe36b575eac4be35b3e639ed027ed5cfad69e24
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-19 16:05:27 +0300

    changed on createHashRouter

--- client/src/routes/UserRouter.tsx
+++ client/src/routes/UserRouter.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import TasksPage from '../pages/system/TasksPage';
 import TaskPage from '../pages/system/TaskPage';
 import AuthPage from '../pages/system/AuthPage';
 import ProfilePage from '../pages/system/ProfilePage';
 import VacationPage from '../pages/system/Vacation';
-import CalendarPage from '../pages/system/CalendarPage';
-import SettingsPage from '../pages/system/SettingsPage';
 import { SystemPage } from '../pages/system/SystemPage/SystemPage';
 import ErrorPage from '../pages/system/ErrorPage';
 
 
-const UserRouter = createBrowserRouter([
+const UserRouter = createHashRouter([
     {
         path: '/',
         errorElement: <ErrorPage title='Error' />,
@@ -24,8 +22,6 @@ const UserRouter = createBrowserRouter([
                     { path: 'tasks', element: <TasksPage title="Задачи"/> },
                     { path: 'tasks/:id', element: <TaskPage title="Задача"/> },
                     { path: 'vacation', element: <VacationPage title="Отпуск"/> },
-                    { path: 'calendar', element: <CalendarPage title="Календарь"/> },
-                    { path: 'settings', element: <SettingsPage title="Настройки"/> },
                 ]
             }
         ],
