003ac1c20f3c7b06a15b526090748677b45f37f7
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-21 12:26:17 +0300

    added create method

--- server/src/controllers/VacationController.ts
+++ server/src/controllers/VacationController.ts
@@ -1,13 +1,26 @@
 import {NextFunction, Request, Response} from 'express';
+import PDF, { EPDFTemplateNames } from '../modules/pdf.js';
+import { VacationCreateSchema } from '../utils/validations.js';
 
 
 class VacationController {
     create = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { start, end } = req.params;
+            const user = req.user!;
+ 
+            const {start, end, days} = await VacationCreateSchema.validate(req.body, {abortEarly: false});
 
+            const templateData = {
+                name: user.name,
+                start,
+                end,
+                days,
+            }
 
-            res.json({});
+            const pdf = new PDF(EPDFTemplateNames.VACATION, user.id, templateData);
+            const pdf_src = await pdf.create();
+ 
+            res.download(pdf_src, 'document.pdf');
         } catch (e) {
             next(e);
         }
